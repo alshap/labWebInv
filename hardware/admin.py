67526a9a1d9988ba42bdacd6cb0a919e2435187c
Alexander <alexander.shaposhnikov@taltech.ee>
2021-05-31 13:44:03 +0100

    admin panel featured

--- hardware/admin.py
+++ hardware/admin.py
@@ -28,6 +28,13 @@ class HardwareAdmin(admin.ModelAdmin):
                 'fields':('product_id', 'image_name')
             }),
         )
+    
+    actions = []
+    def get_actions(self, request):
+        actions = super(HardwareAdmin, self).get_actions(request)
+        del actions['delete_selected']
+        return actions
+    
          
             
             
@@ -46,8 +53,23 @@ class TakenHardwareAdmin(admin.ModelAdmin):
         
         message_bit = "Entries deleted and moved to archieve"
         
-    overwritten_delete_selected.short_description = "Delete selected"
+    overwritten_delete_selected.short_description = "Delete selected and move to archieve"
        
     
-admin.site.register(TakenHardwareArchieve)
-admin.site.register(Category)
+@admin.register(TakenHardwareArchieve)
+class TakenHardwareArchieveAdmin(admin.ModelAdmin):
+    actions = []
+    
+    def get_actions(self, request):
+        actions = super(TakenHardwareArchieveAdmin, self).get_actions(request)
+        del actions['delete_selected']
+        return actions
+  
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    actions = []
+    
+    def get_actions(self, request):
+        actions = super(CategoryAdmin, self).get_actions(request)
+        del actions['delete_selected']
+        return actions
